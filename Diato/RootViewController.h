5ad82a32213af57d08417be249ddc1528f77bbea
Ramon Navarro Bosch <ramon.nb@gmail.com>
2012-02-10 13:31:18 +0100

    Initial Commit

---
+++ Diato/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Diato
+//
+//  Created by Ramon Navarro Bosch on 08/02/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
